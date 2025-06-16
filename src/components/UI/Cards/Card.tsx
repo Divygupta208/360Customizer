1d7250eeb8a5e49b4bd834533b2e77406580c9de
Divy Gupta <divygupta208@gmail.com>
2025-06-16 16:15:36 +0530

    feat/card-badge

--- src/components/UI/Cards/Card.tsx
+++ src/components/UI/Cards/Card.tsx
@@ -22,15 +22,22 @@ const Card: React.FC<CardProps> = ({ value }) => {
   };
 
   return (
-    <div className="bg-white flex gap-2 flex-col items-center text-black border-2 border-black/10 rounded-2xl shadow-gray-600 shadow-2xl min-h-[310px] max-w-[300px] sm:min-h-[360px] sm:h-[310px] sm:w-[40vw] lg:h-[50vh] lg:w-[20vw] md:min-h-[360px] md:w-[40vw]">
-      <div className="w-full h-full">
+    <div className="bg-white flex gap-2 flex-col items-center text-black border-2 border-black/10 rounded-2xl shadow-gray-600 shadow-2xl min-h-[310px] max-w-[300px] sm:min-h-[410px] sm:h-[310px] sm:w-[40vw] lg:h-[60vh] lg:w-[20vw] md:min-h-[430px] md:w-[40vw]">
+      <div className="self-end mr-2">
+        {value.badge && (
+          <span className=" left-16 top-1 align-bottom bg-red-500 text-white text-xs font-semibold px-2 py-1 rounded-md shadow-md z-10">
+            {value.badge}
+          </span>
+        )}
+      </div>
+      <div className="w-full h-full ">
         <img
-          className="w-[70%] mt-3 mx-auto"
+          className="w-[80%] mt-3 mx-auto"
           src={value.imageUrl}
           alt={value.title}
         />
       </div>
-      <div className="flex flex-col items-start md:ml-2 text-ellipsis px-4 py-2 w-[100%] h-full">
+      <div className="flex flex-col items-start md:ml-2 text-ellipsis px-4 py-2 w-[100%]">
         <h6 className="font-bold max-w-[200px] truncate text-start">
           {value.title}
         </h6>
